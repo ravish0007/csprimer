311829bff3ca0ca5818d4d3b9135bb695e69de75
ravish0007 <ravish.nfs@gmail.com>
2025-05-01 08:57:21 +0530

    refactor luhn algorithm

--- programming_beyond_the_basics/introduction/luhn_algorithm/python/luhn_algorithm.py
+++ programming_beyond_the_basics/introduction/luhn_algorithm/python/luhn_algorithm.py
@@ -1,12 +1,14 @@
 def verify(digits):
-    double = lambda num: num * 2 
-    sum_digits = lambda num: sum(map(int, str(num)))
-    double_and_sum = lambda num: sum_digits(double(num))
-    conditional_double_and_sum = lambda tuple : (lambda index, digit: double_and_sum(digit) if index%2 else digit)(tuple[0], int(tuple[1]))
+    def double(num): return num * 2
+    def sum_digits(num): return sum(map(int, str(num)))
+    def double_and_sum(num): return sum_digits(double(num))
+
+    def conditional_double_and_sum(tuple): return (lambda index, digit: double_and_sum(
+        digit) if index % 2 else digit)(tuple[0], int(tuple[1]))
     return sum(map(conditional_double_and_sum, enumerate(digits))) % 10 == 0
 
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     import os
     os.system('clear')
     print('------------')
@@ -14,4 +16,3 @@ if __name__ == '__main__'
     print(verify("17893729974"))
     print('ok')
     print('------------')
-
